55ab0fbe0125b50b7081522f90ae4d1493237abf
BlenderBro <vlad.s.dobrescu@gmail.com>
2018-05-05 15:13:54 +0300

    curatat index si adaugat un fetch reqeust

---
+++ assets/js.js
@@ -0,0 +1,14 @@
+let containerTitlu = document.getElementById('titlu');
+let containerCorp = document.getElementById('corp');
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/45')
+    //server zice: Nu exista Status 404
+    //Server zice: Server down Status 500
+
+    .then(response => response.json())
+    .then(function (response) {
+        console.log(response.title);
+        containerTitlu.innerHTML = response.title;
+        containerCorp.innerHTML = response.body;
+    })
