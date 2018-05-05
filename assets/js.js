9e3848326a55493f097f5c79090bdd44e7fa374c
nistor-serban <vasile.serban.nistor@gmail.com>
2018-05-05 15:53:09 +0300

    am populat cu ajax

--- assets/js.js
+++ assets/js.js
@@ -12,3 +12,14 @@ fetch('https://jsonplaceholder.typicode.com/posts/45')
         containerTitlu.innerHTML = response.title;
         containerCorp.innerHTML = response.body;
     })
+
+    let containerTitluArticol = document.getElementById('titlu-articol');
+    let containerCorpArticol = document.getElementById('corp-articol');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/3')
+    .then(response => response.json())
+    .then(function (response) {
+        console.log(response.title);
+        containerTitluArticol.innerHTML = response.title;
+        containerCorpArticol.innerHTML = response.body;
+    })
